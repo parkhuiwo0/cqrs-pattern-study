05249c092da24d60911d6142b8077a00b3288e02
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-01 14:15:43 +0900

    initialize project

---
+++ ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.cqrs.ProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
---
+++ ProductService/src/test/java/com/parkhuiwo0/cqrs/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.cqrs.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
