909b1cbcc946724a59efaae3f6811b7b4ad3d528
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-03 17:39:46 +0900

    DiscoveryServer (Eureka Application) Configuration 추가

--- DiscoveryServer/src/main/java/com/parkhuiwo0/cqrs/DiscoveryServer/DiscoveryServerApplication.java
+++ DiscoveryServer/src/main/java/com/parkhuiwo0/cqrs/DiscoveryServer/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.parkhuiwo0.cqrs.DiscoveryServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
