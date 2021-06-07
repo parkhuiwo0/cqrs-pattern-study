fdecacabaf9c1f748dcdb29d7ecce74de4033361
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-07 18:12:12 +0900

    ApiGateway Project Initialize

---
+++ ApiGateway/src/test/java/com/parkhuiwo0/cqrs/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.cqrs.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
