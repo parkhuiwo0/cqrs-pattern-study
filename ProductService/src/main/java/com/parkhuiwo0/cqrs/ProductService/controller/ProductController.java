cd95fd83786f9485a312e9d624754cd540549542
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-07 18:30:33 +0900

    [ProductService] - products get api 추가

--- ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.parkhuiwo0.cqrs.ProductService.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/apis/v1/products")
 public class ProductController {
+
+    @GetMapping
+    public String getMethod() {
+        return "products get!";
+    }
 }
