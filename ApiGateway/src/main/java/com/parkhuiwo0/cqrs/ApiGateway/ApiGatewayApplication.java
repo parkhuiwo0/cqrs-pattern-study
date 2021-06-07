4995de02d0fee7d6558ecba9d337a942bc299d43
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-07 18:17:02 +0900

    [ApiGateway] - 유레카 서버에 등록을 위한 @EnableDiscoveryClient 어노테이션 추가

--- ApiGateway/src/main/java/com/parkhuiwo0/cqrs/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/parkhuiwo0/cqrs/ApiGateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.parkhuiwo0.cqrs.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
