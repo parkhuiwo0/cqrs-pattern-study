9cfae2a775711cf9874d3bf4d2c5bbe402a89ae4
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-08 12:04:44 +0900

    로드 밸런싱 테스트를 위한 Environment Portnumber 출력

--- ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
@@ -1,15 +1,20 @@
 package com.parkhuiwo0.cqrs.ProductService.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/apis/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final Environment env;
 
     @GetMapping
     public String getMethod() {
-        return "products get!";
+        return "products get!" + env.getProperty("local.server.port");
     }
 }
