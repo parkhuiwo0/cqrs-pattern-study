283a9c447e73c6b12f6d2856ec1214521d4ef6ac
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-09 17:21:20 +0900

    [ProductService] - CreateProductCommand Command Model 생성 후 CommandGateway로 전송

--- ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.parkhuiwo0.cqrs.ProductService.controller;
 
+import com.parkhuiwo0.cqrs.ProductService.command.CreateProductCommand;
 import com.parkhuiwo0.cqrs.ProductService.domain.request.CreateProductRequest;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/apis/v1/products")
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final Environment env;
+    private final CommandGateway commandGateway;
 
     @GetMapping
     public String getMethod() {
@@ -19,6 +24,23 @@ public class ProductController {
 
     @PostMapping
     public String createProduct(@RequestBody CreateProductRequest request) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(request.getPrice())
+                .quantity(request.getQuantity())
+                .title(request.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            returnValue = e.getLocalizedMessage();
+        }
+
+
         return "product created ! " + request.getTitle();
     }
 }
