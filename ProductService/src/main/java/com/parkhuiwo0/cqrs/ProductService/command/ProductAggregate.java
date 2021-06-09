46aa3eecbc10f308fd7715eb9517c189977451b4
휴/박희우 <parkhuiwo0@gmail.com>
2021-06-09 18:10:20 +0900

    [ProductService] - @EventSourcingHandler 어노테이션을 이용한 ProductCreateEvent 처리

--- ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/command/ProductAggregate.java
+++ ProductService/src/main/java/com/parkhuiwo0/cqrs/ProductService/command/ProductAggregate.java
@@ -2,6 +2,8 @@ package com.parkhuiwo0.cqrs.ProductService.command;
 
 import com.parkhuiwo0.cqrs.ProductService.event.ProductCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.springframework.beans.BeanUtils;
@@ -10,6 +12,12 @@ import java.math.BigDecimal;
 
 @Aggregate
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
 
     public ProductAggregate() {
 
@@ -32,5 +40,15 @@ public class ProductAggregate {
         BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
 
         AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.title = productCreatedEvent.getTitle();
+        this.quantity = productCreatedEvent.getQuantity();
+
+
     }
 }
